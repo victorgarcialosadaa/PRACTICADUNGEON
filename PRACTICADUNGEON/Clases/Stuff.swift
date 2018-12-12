6103b1215918df0c6f1a01b2fb97eda2808a6146
Edgar Doutón <edgar.douton@outlook.com>
2018-12-12 19:59:54 +0100

    dungeon v1

---
+++ PRACTICADUNGEON/Clases/Stuff.swift
@@ -0,0 +1,24 @@
+//
+//  Stuff.swift
+//  PRACTICADUNGEON
+//
+//  Created by DAM on 16/11/2018.
+//  Copyright © 2018 DAM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Stuff{
+    var itemsOnStuff = [String:Items]()
+    var helm:Items?
+    var armor:Items?
+    var weapon:Items?
+    var totalAtk:Int = 0
+    var totalDef:Int = 0
+    var totalMag:Int = 0
+    var totalLuck:Int = 0
+    var img:[UIImage] = []
+    
+  
+    }
